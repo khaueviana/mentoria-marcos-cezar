7e08b5679b5e04e3110823d325e3ecca50857f9d
marcoscezar1 <dungamarcos.paulo@gmail.com>
2017-10-15 20:01:47 -0200

    Estudando iteracacao com for

---
+++ Notes/iteração/iteração/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iteração
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i<10; i++)
+            {
+                //Console.WriteLine(i);
+                if (i == 7)	
+
+                {
+                    Console.WriteLine("Found Seven!");
+                    break;
+                }
+            }
+
+
+            for (int i = 1; i < 12; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+
+            Console.Read();
+        }
+    }
+}
