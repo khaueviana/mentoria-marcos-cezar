d11ba21dcaf00c4941d9a1332f39a4375f056fad
marcoscezar1 <dungamarcos.paulo@gmail.com>
2017-10-21 00:16:57 -0200

    Estudando a biblioteca .Net

---
+++ Notes/AssembliesAndNamesspaces/AssembliesAndNamesspaces/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace AssembliesAndNamesspaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string text = "We want to write this to our file.";
+            File.WriteAllText(@"C:\Users\MARMIG\mentoria-marcos-cezar\Notes\AssembliesAndNamesspaces\text.txt",text);
+
+
+            WebClient client = new WebClient();
+            string reply = client.DownloadString("http://msdn.microsoft.com");
+
+
+            Console.WriteLine(reply);
+            Console.ReadLine();
+        }
+    }
+}
