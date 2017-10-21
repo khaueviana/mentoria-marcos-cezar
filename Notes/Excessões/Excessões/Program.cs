f6329201b97622946f299e4176705716e7ceedcf
marcoscezar1 <dungamarcos.paulo@gmail.com>
2017-10-21 13:43:45 -0200

    Estudando excessoes

---
+++ Notes/Excessões/Excessões/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HandlingExceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string content = File.ReadAllText(@"C:\Lesson22\Exampl.txt");
+                Console.WriteLine(content);
+
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine("There was a problem!");
+                Console.WriteLine("Make sure the name of the file is named correctly: Exampl.txt");
+            }
+            catch (DirectoryNotFoundException ex)
+            {
+                Console.WriteLine("There was a problem!");
+                Console.WriteLine(@"Make sure the directory C:\Lesson22 exists.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("There was a problem!");
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Closing application now ...");
+            }
+            Console.ReadLine();
+        }
+    }
+}
