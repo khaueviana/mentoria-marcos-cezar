6108fa0d43b4d9dfcf9b6632b4d450d6df598495
marcoscezar1 <dungamarcos.paulo@gmail.com>
2017-11-11 09:31:36 -0200

    exercícioNaFaseBeta

---
+++ encontros/#2/Banco/Banco/SeguroDeVida.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banco
+{
+    class SeguroDeVida : ITributavel
+    {
+
+        public double CalculaTributos()
+        {
+            return 42;
+        }
+
+    }
+}
---
+++ encontros/#2/Banco/Banco/TotalizadorDeTributos.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banco
+{
+    class TotalizadorDeTributos
+    {
+
+        public double Total { get; set; }
+        
+        public void Acumula (ITributavel t)
+        {
+            this.Total += t.CalculaTributos();
+        }
+
+    }
+}
