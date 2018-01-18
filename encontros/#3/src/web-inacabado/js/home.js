6c921829cad163355e0d070d7af87383e965b687
marcoscezar1 <dungamarcos.paulo@gmail.com>
2018-01-17 22:20:34 -0200

    comitando

--- encontros/#3/src/web-inacabado/js/home.js
+++ encontros/#3/src/web-inacabado/js/home.js
@@ -1,1 +1,12 @@
-$('.painel').addClass('painel-compacto')
+$('.painel').addClass('painel-compacto');
+
+$('novidades button').click(function(){
+    if($('novidades .painel-compacto li:nth-child(n+7)').css('display') == 'none'){
+        $('novidades .painel-compacto li:nth-child(n+7)').css({'display':'inline-block'})
+        $('button').text('mostrar menos');
+    }
+    else if($('novidades .painel-compacto li:nth-child(n+7)').css('display') != 'none'){  
+        $('novidades .painel-compacto li:nth-child(n+7)').css({'display':'none'})      
+        $('novidades button').text('mostrar mais');
+    }
+});
