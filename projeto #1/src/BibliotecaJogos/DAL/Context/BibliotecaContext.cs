23a1949fe9136a514fa51ab59c58c4157ca62d36
marcoscezar1 <dungamarcos.paulo@gmail.com>
2018-02-24 16:02:37 -0300

    Criando o contexto e setando a string de conexão

---
+++ projeto #1/src/BibliotecaJogos/DAL/Context/BibliotecaContext.cs
@@ -0,0 +1,12 @@
+﻿using Entidades;
+using System.Data.Entity;
+
+namespace DAL.Context
+{
+    public class BibliotecaContext : DbContext
+    {
+        public BibliotecaContext() : base("BibliotecaJogos") { }
+
+        DbSet<Genero> Generos { get; set; }
+    }
+}
