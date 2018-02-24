6f422ad47aae96515e555f64df7b65c03fdfb279
marcoscezar1 <dungamarcos.paulo@gmail.com>
2018-02-24 15:55:06 -0300

    Criando a area genero na camada de UI

---
+++ projeto #1/src/BibliotecaJogos/UI/Areas/Genero/GeneroAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace UI.Areas.Genero
+{
+    public class GeneroAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Genero";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Genero_default",
+                "Genero/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
