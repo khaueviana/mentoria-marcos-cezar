f161cf7868498302ea5cdb72137ef16e7e02ccea
marcoscezar1 <dungamarcos.paulo@gmail.com>
2018-02-24 23:38:03 -0300

    Criando o modelo os modelos restantes

---
+++ projeto #1/src/BibliotecaJogos/Entidades/Jogo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entidades
+{
+    public class Jogo
+    {
+        [Key]
+        public long JogoId { get; set; }
+
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public DateTime DataLancamento{ get; set; }
+        public decimal Preco { get; set; }
+        public byte NotaDoJogo { get; set; }
+
+        [ForeignKey("Genero")]
+        public long GeneroId { get; set; }
+        public Genero Genero { get; set; }
+
+        [ForeignKey("Plataforma")]
+        public long PlataformaId { get; set; }
+        public Plataforma Plataforma { get; set; }
+
+        [ForeignKey("Produtora")]
+        public long ProdutoraId { get; set; }
+        public Produtora Produtora { get; set; }
+    }
+}
---
+++ projeto #1/src/BibliotecaJogos/Entidades/Produtora.cs
@@ -0,0 +1,8 @@
+﻿namespace Entidades
+{
+    public class Produtora
+    {
+        public long  ProdutoraId { get; set; }
+        public string Nome { get; set; }
+    }
+}
